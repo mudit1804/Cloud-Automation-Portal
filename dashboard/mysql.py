a3c1876b09ad0fd10c51813e23266ea64aef28c3
Mudit Mehta <muditbudy@gmail.com>
2018-07-31 12:17:25 +0530

    Initial code commitC

---
+++ dashboard/mysql.py
@@ -0,0 +1,46 @@
+import MySQLdb
+def buildMysql(host,user,passwd,uname,upwd,udb):
+    
+    try:
+        mydb = MySQLdb.connect(host=host, port=3306,user=user, passwd=passwd)
+        print "hello Connection Successfull"
+        cursor = mydb.cursor()
+        mkuser = uname
+        mkpass = upwd
+        dbname = udb
+        dbcreation = "CREATE DATABASE %s" %(dbname)
+        print dbcreation
+        results = cursor.execute(dbcreation)
+        print "DB creation returned", results
+        creation = "CREATE USER '%s'@'%s' IDENTIFIED BY '%s'" %(mkuser,'%',mkpass)
+        print creation
+        results = cursor.execute(creation)
+        print "User creation at world", results
+        creation = "CREATE USER '%s'@'%s' IDENTIFIED BY '%s'" %(mkuser,'localhost',mkpass)
+        print creation
+        results = cursor.execute(creation)
+        #print "User creation at local", results
+        #mkpass = upwd
+        #setpass = "SET PASSWORD FOR '%s'@'%s' = PASSWORD('%s')" %(mkuser, '%', mkpass)
+        #results = cursor.execute(setpass)
+        #print "Setting of password returned", results
+        granting = "GRANT ALL ON %s.* TO '%s'@'%s'" %(dbname, mkuser,'localhost')
+        print granting
+        results = cursor.execute(granting)
+        print "Granting of privileges localhost", results
+        granting = "GRANT ALL ON %s.* TO '%s'@'%s'" %(dbname, mkuser,'%')
+        print granting
+        results = cursor.execute(granting)
+        print "Granting of privileges wporld", results  
+        flushprivilege = "FLUSH PRIVILEGES"
+        print flushprivilege  
+        results = cursor.execute(flushprivilege)
+        print "Flushing", results    
+        res = 't'
+        return res
+    except MySQLdb.Error, e:
+
+        res = 'f'
+        print e
+        return res
+
